31c01e150f67d725f2403e78f17677380db8ddda
tao.ranting <tao.ranting@chinaredstar.com>
2023-10-30 11:25:27 +0800

    .

--- src/views/training/knowledge/index.vue
+++ src/views/training/knowledge/index.vue
@@ -1,5 +1,15 @@
 <template>
   <ContentLayout :key="route.name" class="bg-white pt-8">
+    <h3 class="text-xl leading-6 font-medium mb-4">{{ $t('我的知识库') }}</h3>
+    <p class="text-[#596780] text-sm leading-5 mb-6">
+      {{ $t('知识库中可添加问答或文档，在机器人回答时，可运用库中的知识进行回复。此外还能') }}
+      <el-button type="primary" link @click="relatedModalVisible = true">
+        <template #icon>
+          <svg-icon name="related" svg-class="w-4 h-4" />
+        </template>
+        {{ $t('关联其他知识库') }}
+      </el-button>
+    </p>
     <el-tabs
       type="card"
       :model-value="activeTab"
@@ -10,12 +20,18 @@
     </el-tabs>
     <component :is="tabComponents[activeTab]" />
   </ContentLayout>
+  <Modal v-model:visible="relatedModalVisible" title="关联其他知识库">
+    <p class="text-[#596780] text-xs leading-4">
+      {{ $t('让机器人回答你的机器人回答时，能动态索引其他知识库的内容') }}
+    </p>
+  </Modal>
 </template>
 
 <script setup lang="ts">
+import Modal from '@/components/Modal/index.vue'
 import ContentLayout from '@/layout/ContentLayout.vue'
 import { RoutesMap } from '@/router'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import DocView from './components/DocView.vue'
 import QAView from './components/QAView.vue'
@@ -38,4 +54,6 @@ const tabComponents = {
 const onClickTab = (v) => {
   router.push({ name: RoutesMap.tranning.knowledge, params: { type: v } })
 }
+
+const relatedModalVisible = ref(false)
 </script>
