38f9778c06e446d7698ea60455e4d25185378334
tao.ranting <tao.ranting@chinaredstar.com>
2023-11-03 10:20:38 +0800

    feat: chat guide

--- src/api/industry.ts
+++ src/api/industry.ts
@@ -1,17 +1,22 @@
+import type { IDomainInfo } from '@/interface/domain'
 import request from '@/utils/request'
-import type { IFirstGuideIndustry } from '@/interface/industry'
 
-// 新用户注册表单-提交
-export function saveFirstGuideIndustry(data: IFirstGuideIndustry) {
-  return request<IFirstGuideIndustry>({
+// 新用户引导-提交
+export function saveFirstGuideAdditions(data: any) {
+  return request({
     method: 'post',
     url: '/chato/api/v1/org/additions/save',
     data
   })
 }
 
-// 新注册用户表单-行业+问题
-// getFirstGuideSelect
+// 新用户引导-机器人推荐
+export function getFirstGuideInterestDomains() {
+  return request<IDomainInfo[]>({
+    url: '/chato/api/v2/domains/interests'
+  })
+}
+
 export function getFirstGuideIndustry() {
   return request({
     url: '/chato/api/v1/org/industry_select_config'
