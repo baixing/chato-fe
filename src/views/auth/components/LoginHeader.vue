3b7c512c111c37f4f36a6666a141599c48a611fc
erkuan <86552605+lik778@users.noreply.github.com>
2023-11-14 06:16:52 -0600

    fix: login chato  logo size (#195)
    Co-authored-by: 李款 <likuan@baixing.com>

--- src/views/auth/components/LoginHeader.vue
+++ src/views/auth/components/LoginHeader.vue
@@ -1,9 +1,14 @@
 <template>
   <header class="flex justify-center items-center flex-wrap">
-    <img src="@/assets/img/chato-animation.gif" alt="" class="mx-2 w-[194px] h-[40px]" />
+    <img src="@/assets/img/chato-animation.gif" alt="" class="mx-2 h-[60px] md:h-[50px]" />
   </header>
-  <div class="mt-4 text-center text-lg text-[#3d3d3d]">
+  <div class="mt-[32px] text-center text-[#3d3d3d] text-base">
     <p>{{ $t('基于 AI 技术轻松创建对话机器人') }}</p>
-    <p>{{ $t('限时永久免费 赠送100电力值') }}</p>
+    <p>
+      {{ $t('限时') }}
+      <span class="text-[#596780] font-medium"> {{ $t('免费赠送') }} </span>
+      100
+      {{ $t('电力值') }}
+    </p>
   </div>
 </template>
