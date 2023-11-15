21150e7051d3dd3bb37b378d156053b0225ea484
pieceof <wangyifeng@baixing.com>
2023-11-15 20:03:58 +0800

    feat: change login view

--- src/views/auth/components/LoginHeader.vue
+++ src/views/auth/components/LoginHeader.vue
@@ -3,7 +3,7 @@
     <img src="@/assets/img/chato-animation.gif" alt="" class="mx-2 h-[60px] md:h-[50px]" />
   </header>
   <div class="mt-[32px] text-center text-[#3d3d3d] text-base">
-    <p>{{ $t('基于 AI 技术轻松创建对话机器人') }}</p>
+    <!--    <p>{{ $t('基于 AI 技术轻松创建对话机器人') }}</p>-->
     <p>
       {{ $t('限时') }}
       <span class="text-[#596780] font-medium"> {{ $t('免费赠送') }} </span>
