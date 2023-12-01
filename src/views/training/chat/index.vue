57931cee56d8b20391ac0b6d40c5e28e1f976297
wangzichu <87469919+WangZiChu199910252255@users.noreply.github.com>
2023-12-01 11:11:17 +0800

    style: debugchat style (#271)

--- src/views/training/chat/index.vue
+++ src/views/training/chat/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="bg-white h-[calc(100%-56px)]">
-    <BotProgress />
-    <DebugChat class="h-full mx-auto !px-[18%] lg:!px-4" />
+  <div class="bg-white h-full max-h-[calc(100%-56px)]">
+    <div class="h-full mx-auto !px-[18%] lg:!px-4 flex flex-col">
+      <BotProgress />
+      <DebugChat class="flex-1 overflow-y-auto" />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
