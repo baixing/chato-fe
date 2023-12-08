7099591ba52ebe0e02a612b2b205f6b8e6863727
李款 <likuan@baixing.com>
2023-12-08 14:04:24 +0800

    feat: 增加抖音回传api

--- src/views/chating/ChatItem.vue
+++ src/views/chating/ChatItem.vue
@@ -37,8 +37,10 @@ import { EDocumentTabType } from '@/enum/knowledge'
 import { useDomainStore } from '@/stores/domain'
 import { replaceMarkdownUrl } from '@/utils/help'
 import { removewRegReplaceA } from '@/utils/reg'
+import dayjs from 'dayjs'
 import { storeToRefs } from 'pinia'
-import { computed, reactive, ref } from 'vue'
+import { computed, reactive, ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
 
 withDefaults(
   defineProps<{
@@ -51,6 +53,7 @@ withDefaults(
   }
 )
 
+const route = useRoute()
 const domainStoreI = useDomainStore()
 const { domainInfo } = storeToRefs(domainStoreI)
 const dialogVisibleQa = ref(false)
@@ -83,4 +86,34 @@ const onOpenSource = (question_id, slug) => {
   currentDrawer.slug = slug
   currentDrawer.visible = true
 }
+
+// 抖音api回调
+const onDouyinAPIClick = (id) => {
+  fetch('https://analytics.oceanengine.com/api/v2/conversion', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' // 告诉服务器我们正在发送JSON数据
+    },
+    body: JSON.stringify({
+      event_type: 'active',
+      context: {
+        ad: {
+          callback: id
+        }
+      },
+      timestamp: dayjs().valueOf()
+    })
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error('Error:', error))
+}
+
+watch(
+  () => route.query.clickid,
+  (v) => {
+    v && onDouyinAPIClick(v)
+  },
+  { immediate: true }
+)
 </script>
