b087cbc7fc87c2347fce9eec8d4ce9d8f01bf0d2
李款 <likuan@baixing.com>
2023-12-15 11:02:13 +0800

    feat: 抖音小程序跳转页面

---
+++ src/utils/callback.ts
@@ -0,0 +1,47 @@
+import dayjs from 'dayjs'
+
+// 抖音回传
+export const douYinCallbackAPI = (id, callback) => {
+  callback()
+  fetch('/api/v2/conversion', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' // 告诉服务器我们正在发送JSON数据
+    },
+    body: JSON.stringify({
+      event_type: 'active_register',
+      context: {
+        ad: {
+          callback: id
+        }
+      },
+      timestamp: dayjs().valueOf()
+    })
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error('Error:', error))
+}
+
+// 百度api回调
+export const baiduCallbackAPI = (callback) => {
+  callback()
+  fetch('/ocpcapi/api/uploadConvertData', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' // 告诉服务器我们正在发送JSON数据
+    },
+    body: JSON.stringify({
+      token: 'WHp4OdQwcDQA4OlqFc9sZ63PgKFiCBBI@GqHeOYUewGK8GO4OQuFcXq8uFI8KUcp4',
+      conversionTypes: [
+        {
+          logidUrl: location.href,
+          newType: 25
+        }
+      ]
+    })
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error('Error:', error))
+}
---
+++ src/views/naviagte/Redirect.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>正在跳转阿旺小程序...</div>
+</template>
+
+<script setup lang="ts">
+import useClickId from '@/composables/useClickId'
+import { douYinCallbackAPI } from '@/utils/callback'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const { clickId } = useClickId(route)
+
+const init = () => {
+  try {
+    clickId.value && douYinCallbackAPI(clickId.value, () => {})
+  } catch (error) {
+  } finally {
+    location.href = 'https://admin.sdlian.cn/l/hBlc2rImLp'
+  }
+}
+
+init()
+</script>
+
+<style scoped></style>
