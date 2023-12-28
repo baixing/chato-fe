e9bc65971c76efa40938377c25db47f1d6f86cbc
李款 <likuan@baixing.com>
2023-12-28 14:03:34 +0800

    fix: load default  title

--- src/composables/usePageTitle.ts
+++ src/composables/usePageTitle.ts
@@ -1,15 +1,13 @@
-import { FULL_SITE_NAME, SITE_NAME } from '@/constant/common'
+import { SITE_NAME } from '@/constant/common'
 
 export default function usePageTitle(titleSegments, { trimTail = false } = {}) {
   if (!Array.isArray(titleSegments)) titleSegments = [titleSegments]
   titleSegments = titleSegments.filter(Boolean)
 
   let result
-  if (titleSegments.length === 0) {
-    result = FULL_SITE_NAME
-  } else {
+  if (titleSegments.length !== 0) {
     if (!trimTail) titleSegments.push(SITE_NAME)
     result = titleSegments.join(' - ')
+    document.title = result
   }
-  document.title = result
 }
