41207473221ea2c43f0de27be3966dc56882f564
李款 <likuan@baixing.com>
2024-01-24 14:37:43 +0800

    refactor: get api 30%

--- src/api/graph.ts
+++ src/api/graph.ts
@@ -1,9 +1,6 @@
 import request from '@/utils/request'
 
-export function getCommonGraph<R, P extends Record<string, unknown> = Record<string, unknown>>(
-  suffix: string,
-  params?: P
-) {
+export function getCommonGraph<R, P extends object = object>(suffix: string, params?: P) {
   return request<R>({
     url: `/chato/api/v1/graph/${suffix}`,
     params: params ?? {}
