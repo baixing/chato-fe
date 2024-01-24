a89f741c616cbca98ef6a8f39dd310b990a514c4
15202151025 <361962182@qq.com>
2024-01-24 17:26:24 +0800

    refactor: api

--- src/views/guide/firstGuide.vue
+++ src/views/guide/firstGuide.vue
@@ -215,6 +215,7 @@ const onGotoCreate = async () => {
       })
       baseStoreI.getUserInfo()
     }
+    console.log(userInfo.value)
     const {
       data: { data }
     } = await postCommonGraph<IDomainInfo>('chato_domains/save', {
