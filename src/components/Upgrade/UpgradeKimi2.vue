d741a28ea1d66dc63e64ef7628de952acf58d2d9
Jiangzhou Li <605858621@qq.com>
2024-04-23 10:05:36 +0800

    fix: kimi callback baidu

--- src/components/Upgrade/UpgradeKimi2.vue
+++ src/components/Upgrade/UpgradeKimi2.vue
@@ -5,10 +5,22 @@
 </template>
 
 <script>
+import useBaidu from '@/composables/useBaidu'
+import { baiduCallbackAPI } from '@/utils/callback'
+import { useRoute } from 'vue-router'
+import { useStorage } from '@vueuse/core'
+
+const route = useRoute()
+const { bdvid } = useBaidu(route)
+const baiduAPI = useStorage('baiduAPI', false)
+
 export default {
   name: 'RedirectPage',
 
   mounted() {
+    if (bdvid.value && !baiduAPI.value) {
+      baiduCallbackAPI(bdvid.value, () => (baiduAPI.value = true))
+    }
     // 你希望用户跳转去的外部链接
     const externalUrl = `http://kimi.moonshot.cn/?tr_param1=aiwriting&tr_param2=newpage&Campaign_group_id=baixing&prefill_prompt=hello&utm_source=Chat&utm_campaign=baixing_kimi&utm_content=到kimi，体验更多AI智能写作场景`
     // 用 replace 方法替换当前页面
