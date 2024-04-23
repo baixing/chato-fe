153647f1d79f748f38a264b37868ad0075e9c505
Jiangzhou Li <605858621@qq.com>
2024-04-23 09:56:18 +0800

    feat: 50% redirect to kimi

---
+++ src/components/Upgrade/UpgradeKimi2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- 页面内容，实际上用户可能看不到，因为页面将被重定向 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RedirectPage',
+
+  mounted() {
+    // 你希望用户跳转去的外部链接
+    const externalUrl = `http://kimi.moonshot.cn/?tr_param1=aiwriting&tr_param2=newpage&Campaign_group_id=baixing&prefill_prompt=hello&utm_source=Chat&utm_campaign=baixing_kimi&utm_content=到kimi，体验更多AI智能写作场景`
+    // 用 replace 方法替换当前页面
+    window.location.replace(externalUrl)
+  }
+}
+</script>
