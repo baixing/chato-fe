49a1a68f76befecc116102622c04ea1ad13ed82a
李款 <likuan@baixing.com>
2024-07-02 16:23:28 +0800

    fix: update router

--- src/router/index.ts
+++ src/router/index.ts
@@ -485,9 +485,9 @@ const loginedRoutes = [
       ...managerRoutes, // 管理机器人
       ...resourceSquareRoutes, // 资源广场
       ...aiPluginSquareRoutes, // AI插件库
-      ...spaceManager,
-      ...flowManager,
-      ...activityManager
+      ...spaceManager
+      // ...flowManager,
+      // ...activityManager
       // ...vipManager
     ]
   }
